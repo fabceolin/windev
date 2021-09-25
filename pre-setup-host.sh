31921addbc1d154c01fdd34d17e4c6e50c8eb17e
Fabrício Ceolin <fabceolin@gmail.com>
2021-09-25 02:09:42 +0000

    adding docker installation

--- pre-setup-host.sh
+++ pre-setup-host.sh
@@ -3,7 +3,7 @@
 # Script to install python3-pip, sshpass and ansible on the host machine
 
 apt-get update
-apt-get install -y python3-pip sshpass
+apt-get install -y python3-pip sshpass sudo
 
 python3 -m pip install pip --upgrade
 python3 -m pip install ansible
