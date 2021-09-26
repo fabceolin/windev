cf5cd0e25acf366dfa503e96f24d41a337954d05
Fabricio Ceolin <fabceolin@gmail.com>
2021-09-26 20:27:01 +0000

    Using debootstrap on pre-setup-host

--- pre-setup-host.sh
+++ pre-setup-host.sh
@@ -3,6 +3,11 @@
 # Script to install python3-pip, sshpass and ansible on the host machine
 
 # Considering the mininum installation after focal debotstrapped chroot variant buildd
+mkdir windev
+sudo debootstrap --variant=buildd focal windev
+for i in dev proc sys /dev/pts do mount --bind /$i windows/$i; done
+chroot windev <<EOF
+cd /root
 apt-get update
 apt-get upgrade -y
 apt-get install -y sshpass sudo python3 python3-distutils wget
@@ -11,3 +16,4 @@ python3 get-pip.py
 
 python3 -m pip install pip --upgrade
 python3 -m pip install ansible
+EOF
