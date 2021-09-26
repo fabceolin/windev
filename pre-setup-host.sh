44e6a4f7f92d8cfee9e213a5ed4839917978e82a
Fabricio Ceolin <fabceolin@gmail.com>
2021-09-26 11:26:44 +0000

    Improving setup from fresh chroot

--- pre-setup-host.sh
+++ pre-setup-host.sh
@@ -2,8 +2,12 @@
 
 # Script to install python3-pip, sshpass and ansible on the host machine
 
+# Considering the mininum installation after focal debotstrapped chroot variant buildd
 apt-get update
-apt-get install -y python3-pip sshpass sudo
+apt-get upgrade -y
+apt-get install -y sshpass sudo python3 python3-distutils wget
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
 
 python3 -m pip install pip --upgrade
 python3 -m pip install ansible
