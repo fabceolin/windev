c37c3bbd9640187d2b55ab35b27bbc880c5aa6fb
Fabrício Ceolin <ceolin@ulevel.com>
2021-09-26 22:16:25 -0300

    Adjusting files

--- pre-setup-host.sh
+++ pre-setup-host.sh
@@ -5,15 +5,16 @@
 # Considering the mininum installation after focal debotstrapped chroot variant buildd
 mkdir windev
 sudo debootstrap --variant=buildd focal windev
-for i in dev proc sys /dev/pts do mount --bind /$i windows/$i; done
+for i in dev proc sys /dev/pts; do mount --bind /$i windev/$i; done
 chroot windev <<EOF
 cd /root
 apt-get update
 apt-get upgrade -y
-apt-get install -y sshpass sudo python3 python3-distutils wget
-wget https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py
+apt-get install software-properties-common
+add-apt-repository multiverse
+add-apt-repository restriced
 
+apt-get install -y sshpass sudo python3 python3-distutils wget python3-pip
 python3 -m pip install pip --upgrade
 python3 -m pip install ansible
 EOF
