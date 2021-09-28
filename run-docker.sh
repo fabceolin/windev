0ee7c9484babdbb3a0fb22cacc6126f784ad99ff
Fabrício Ceolin <fabricio.ceolin@miningmath.com>
2021-09-28 14:02:07 -0300

    Opening vnc port

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,2 @@
-docker run  -p 3389:3389 -p 32022:22 -eCPU=4 -eRAM=4096 --privileged -it --name windev --device=/dev/kvm --device=/dev/net/tun -v /sys/fs/cgroup:/sys/fs/cgroup:rw --cap-add=NET_ADMIN --cap-add=SYS_ADMIN --cap-add=DAC_READ_SEARCH -v /lib/modules/:/lib/modules/ ghcr.io/fabceolin/windev:latest bash
+docker run -p 5900:5900 -p 3389:3389 -p 32022:22 -eCPU=4 -eRAM=4096 --privileged -it --name windev --device=/dev/kvm --device=/dev/net/tun -v /sys/fs/cgroup:/sys/fs/cgroup:rw --cap-add=NET_ADMIN --cap-add=SYS_ADMIN --cap-add=DAC_READ_SEARCH -v /lib/modules/:/lib/modules/ ghcr.io/fabceolin/windev:latest bash
 
